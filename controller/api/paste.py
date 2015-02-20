a301545c9d451cb0fa1ed5ae239ac8479782664a
Tim Foerster <github@1n3t.de>
2015-02-20 01:07:38 +0100

    fixed naming

--- controller/api/paste.py
+++ controller/api/paste.py
@@ -6,17 +6,17 @@ class PasteAPI(Resource,BaseAPI):
     def get(self, key=None):
         if key:
             try:
-                return Response(Paste.find(key).json(), 
+                return Response(Code.find(key).json(), 
                                 mimetype="application/json")
             except:
                 return self._not_found(key)
-        return Response(Paste.all_json(), mimetype="application/json")
+        return Response(Code.all_json(), mimetype="application/json")
 
     def post(self):
         req = request.get_json()
         try:
-            code = Paste.new(req['data'],req['private'])
-            return Response(Paste.find(code.key).json(), 
+            code = Code.new(req['data'],req['private'])
+            return Response(Code.find(code.key).json(), 
                             mimetype="application/json")
         except:
             return self._error()
